1c2e98b9e63f6dd14dd7df48a316dcbd80d2f8da
Miguel Medina Ballesteros <maximetinu@gmail.com>
2018-10-26 12:59:15 +0200

    event system

---
+++ Event System With Scriptable Objects/Editor/GameEventInspector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameEvents
+{
+
+    [CustomEditor(typeof(GameEvent))]
+    public class GameEventInspector : Editor
+    {
+
+        GameEvent thisEvent;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            thisEvent = target as GameEvent;
+
+            GUI.enabled = Application.isPlaying;
+
+            if (GUILayout.Button("Raise event"))
+            {
+                thisEvent.Raise();
+            }
+            GUILayout.Label("Listeners:", EditorStyles.boldLabel);
+            if (Application.isPlaying)
+            {
+                GUILayout.Space(10);
+                GUILayout.Label(thisEvent.ListenersToString());
+            }
+            else
+            {
+                GUILayout.Label("Only in play mode");
+            }
+        }
+    }
+}
